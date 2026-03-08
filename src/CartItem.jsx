9c40a92039b8832ae5c0efa05f03a30bda9ab4fe
Gael <meliarchille@gmail.com>
2026-03-07 23:35:51 -0500

    Update the cart

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0 
+    cart.map((cartItem)=> {
+        total+= cartItem.quanity * parseFloat(item.cost.substring(1))
+    })
+
+    return total
   };
 
   const handleContinueShopping = (e) => {
-   
+    if (onContinueShopping) 
+        onContinueShopping()
   };
 
-
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity:item.quanity+1}))
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({name: item.name, quantity:item.quanity-1}))
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity * parseFloat(item.cost.substring(1))
   };
 
   return (
